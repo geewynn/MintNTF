7f16c056aaf6b8c3f93c5c67b6b3a4dcc23795d5
Geewynn <ekainugodwin@gmail.com>
2021-08-30 16:07:38 +0100

    nft mininting files

---
+++ nft-test/packages/react-app/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { PageHeader } from "antd";
+import React from "react";
+
+// displays a page header
+
+export default function Header() {
+  return (
+      <PageHeader
+        title="NFT MInt"
+        style={{ cursor: "pointer" }}
+      />
+  );
+}
