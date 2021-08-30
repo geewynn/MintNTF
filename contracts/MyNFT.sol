18a4d7a2a560daf13bd90678ad375e82e03f73ab
Geewynn <ekainugodwin@gmail.com>
2021-08-30 10:44:09 +0100

    feat: create nft contract

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() public ERC721("MintNFT", "NFT") {}
+
+    function mintNFT(address recipient, string memory uri)
+        public onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        uri = tokenURI(newItemId);
+
+        return newItemId;
+    }
+}
